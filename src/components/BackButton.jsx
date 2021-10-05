13c86301497348534d66e5505ab44f8d72e774da
chimny <aleks713@gmail.com>
2021-10-05 19:25:53 +0200

    update backbutton

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,14 +1,11 @@
 import {useDispatch} from "react-redux";
 import {removeNumber} from '../redux/slices/inputSlice'
 
-export const BackButton = ()=>{
+export const BackButton = () => {
     const dispatch = useDispatch();
 
-    function exec(){
-        dispatch(removeNumber())
-    }
 
-    return(
-        <button onClick={()=> exec()} > Delete</button>
+    return (
+        <button onClick={() => dispatch(removeNumber())}>Delete</button>
     )
 }
