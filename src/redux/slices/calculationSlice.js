01c3eac57540eb5daaa41824afd50342a8f47c9e
chimny <aleks713@gmail.com>
2021-10-05 17:21:24 +0200

    done

--- src/redux/slices/calculationSlice.js
+++ src/redux/slices/calculationSlice.js
@@ -1,17 +1,21 @@
 import {createSlice} from "@reduxjs/toolkit";
-const initialState = {firstNumber:null,operator:null,secondNumber:null};
 
-
+const initialState = {
+    firstNumber: null,
+    operator: null,
+    secondNumber: null
+};
 
 
 export const calculationSlice = createSlice({
     name: 'calculationSlice',
     initialState,
     reducers: {
-  addElement: (state,action)=>{
-      const prevState = state.firstNumber
-      state.firstNumber = action
-  }
+        //@todo update operator regarding if it's empty or not
+        addElement: (state, action) => {
+            const prevState = {...state}
+
+        }
     }
 })
 
