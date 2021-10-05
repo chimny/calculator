c39f0a6856454adc45eaef0aa431dcd122822d6f
chimny <aleks713@gmail.com>
2021-10-05 19:30:48 +0200

    refactor in dispatch functions

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,10 @@
 import {useDispatch} from "react-redux";
 import {addNumber} from '../redux/slices/inputSlice'
 
-export const Button = (props)=>{
+export const Button = (props) => {
     const dispatch = useDispatch();
 
-    const updateFunction = (argument) => {
-        dispatch(addNumber(argument));
-    }
-
-
-    return(
-        <button onClick={()=>updateFunction(props.number)}>{props.number}</button>
+    return (
+        <button onClick={() => dispatch(addNumber(props.number))}>{props.number}</button>
     )
 }
