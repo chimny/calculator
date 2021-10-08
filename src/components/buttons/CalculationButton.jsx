e804b4ae6d3fc4c5ef89bd4507f61c43df329ff9
chimny <aleks713@gmail.com>
2021-10-08 17:29:49 +0200

    result working properly

--- src/components/buttons/CalculationButton.jsx
+++ src/components/buttons/CalculationButton.jsx
@@ -1,16 +1,19 @@
 import {useSelector, useDispatch} from "react-redux";
-import {numberCalcValue, addOperator,operationSymbols} from '../../redux/slices/calculationSlice'
-import {clearValue} from '../../redux/slices/inputSlice'
+import {numberCalcValue, addOperator,operationSymbols,giveResult} from '../../redux/slices/calculationSlice';
+import {clearValue} from '../../redux/slices/inputSlice';
 
 export const CalculationButton = (props) => {
     const inputValue = useSelector((state => state.inputValue.value));
-    const {firstNumber,secondNumber} = useSelector((state => state.operationSequence));
+    const {firstNumber,operator,secondNumber} = useSelector((state => state.operationSequence));
     const dispatch = useDispatch();
 
 
     function updateInput() {
 if(secondNumber){
     dispatch(operationSymbols())
+}
+if(operator === '='){
+    dispatch(giveResult())
 }
         dispatch(numberCalcValue(inputValue));
         dispatch(addOperator(props.symbol))
--- src/redux/slices/calculationSlice.js
+++ src/redux/slices/calculationSlice.js
@@ -1,60 +1,69 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-    firstNumber: null,
-    operator: null,
-    secondNumber: null,
+    firstNumber: '12',
+    operator: '+',
+    secondNumber: '4',
     result: null
 };
 
 export const calculationSlice = createSlice({
-    name: 'calculationSlice',
-    initialState,
-    reducers: {
-        numberCalcValue(state, action) {
-            if (state.firstNumber === null) {
-                state.firstNumber = action.payload
-            } else {
-                state.secondNumber = action.payload
-            }
-        },
-        addOperator(state, action) {
-            state.operator = action.payload
-        },
-        operationSymbols(state) {
-            const {firstNumber, operator, secondNumber} = state;
-            const firstNumberEnc = Number(firstNumber);
-            const secondNumberEnc = Number(secondNumber);
-            switch (operator) {
-                case '+':
-                    state.firstNumber = state.result;
-                    state.result = firstNumberEnc + secondNumberEnc;
-                    break;
-                case '-':
-                    state.firstNumber = state.result;
-                    state.result = firstNumberEnc - secondNumberEnc;
-                    break;
-                case '*':
-                    state.firstNumber = state.result;
-                    state.result = firstNumberEnc * secondNumberEnc;
-                    break;
-                case '/':
-                    state.firstNumber = state.result;
-                    state.result = firstNumberEnc / secondNumberEnc;
-                    break;
-                case '=':
-                    state.result = `=${state.result}`;
-                    break;
-                default:
-                    return;
-            }
-        }
+        name: 'calculationSlice',
+        initialState,
+        reducers: {
+            numberCalcValue(state, action) {
+                if (state.firstNumber === null) {
+                    state.firstNumber = action.payload
+                } else if(state.secondNumber === null) {
+                    state.secondNumber = action.payload
+                }
+            },
+            addOperator(state, action) {
+                if (action.payload === '=') return
+                state.operator = action.payload
+            },
+            operationSymbols(state) {
+                const {firstNumber, operator, secondNumber} = state;
+                const firstNumberEnc = Number(firstNumber);
+                const secondNumberEnc = Number(secondNumber);
+                switch (operator) {
+                    case '+':
+                        // state.firstNumber = state.result;
+                        state.result = firstNumberEnc + secondNumberEnc;
+                        break;
+                    case '-':
+                        state.firstNumber = state.result;
+                        state.result = firstNumberEnc - secondNumberEnc;
+                        break;
+                    case '*':
+                        state.firstNumber = state.result;
+                        state.result = firstNumberEnc * secondNumberEnc;
+                        break;
+                    case '/':
+                        state.firstNumber = state.result;
+                        state.result = firstNumberEnc / secondNumberEnc;
+                        break;
+                    default:
+                        return;
+                }
+            },
+            giveResult(state) {
+                const {firstNumber, secondNumber, operator} = state;
+                const firstNumberEnc = Number(firstNumber);
+                const secondNumberEnc = Number(secondNumber);
+                state.firstNumber = firstNumberEnc;
+                state.secondNumber = secondNumber;
+                state.operator = operator
+                state.result = firstNumberEnc + secondNumberEnc;
 
+
+            }
+        },
     }
-})
+)
 
 
-export const {numberCalcValue, addOperator,operationSymbols} = calculationSlice.actions;
+export const {numberCalcValue, addOperator, operationSymbols, giveResult} = calculationSlice.actions;
 export default calculationSlice.reducer;
 
 
