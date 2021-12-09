7765e02ff2cc60e1ef0bd33e94bfc351453f1953
chimny <aleks713@gmail.com>
2021-12-09 09:31:58 +0100

    todo added

--- src/redux/slices/themeSlice.js
+++ src/redux/slices/themeSlice.js
@@ -3,9 +3,9 @@ import {firstTheme, secondTheme, thirdTheme} from '../../data/theme';
 
 
 const initialState = [
-        {firstThemeActive: true, firstTheme},
-        {secondThemeActive: false, secondTheme},
-        {thirdThemeActive: false, thirdTheme},
+        {active: true, firstTheme},
+        {active: false, secondTheme},
+        {active: false, thirdTheme},
     ]
 ;
 
@@ -15,21 +15,26 @@ export const themeSlice = createSlice({
     reducers: {
         setTheme(state, action) {
 
-            //@todo clear state restore to set proper object in array
-            const clearedState = {
-                ...Object.keys(state).reduce((reduced, key) => ({...reduced, [0]: false}), {})
-            }
 
+            const clearedState = {
+                ...Object.keys(state).reduce((reduced, key) => ({...reduced, [0]: false}))
+            }
+            //@todo download current state, modify first value as true if particular button is clicked(function) rest should be negative. If unexpected button is pressed, don't change current state
+            const newState = {
+                ...Object.keys(state).reduce((element,index)=>{
+                    return({...element,[0]:false})
+                })
+            }
 
             switch (action.payload) {
                 case 'firstTheme':
-                    return {...state, firstThemeActive: true}
+                   return {...newState}
                 case 'secondTheme':
-                    return {...clearedState, secondThemeActive: true}
+                    return {...state, secondThemeActive: true}
                 case 'thirdTheme':
-                    return {...clearedState, thirdThemeActive: true}
+                    return {...state, thirdThemeActive: true}
                 default:
-                    return {...clearedState, firstThemeActive: true}
+                    return {...state, firstThemeActive: true}
             }
         }
 
