947e4293df87baf3279d1ad737fcb8eb5cf4be08
chimny <aleks713@gmail.com>
2021-12-29 23:54:29 +0100

    jest test setup

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,48 @@ test('renders learn react link', () => {
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
+
+import reducer, { todoAdded } from './redux/slices/testSlice'
+
+test('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual([
+        {
+            text: 'Use Redux',
+            completed: false,
+            id: 0
+        }
+    ])
+})
+
+test('should handle a todo being added to an empty list', () => {
+    const previousState = []
+    expect(reducer(previousState, todoAdded('Run the tests'))).toEqual([
+        {
+            text: 'Run the tests',
+            completed: false,
+            id: 0
+        }
+    ])
+})
+
+test('should handle a todo being added to an existing list', () => {
+    const previousState = [
+        {
+            text: 'Run the tests',
+            completed: true,
+            id: 0
+        }
+    ]
+    expect(reducer(previousState, todoAdded('Use Redux'))).toEqual([
+        {
+            text: 'Run the tests',
+            completed: true,
+            id: 0
+        },
+        {
+            text: 'Use Redux',
+            completed: false,
+            id: 1
+        }
+    ])
+})
