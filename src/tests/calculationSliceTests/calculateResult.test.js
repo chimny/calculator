108146989bf5c6b871bd004c3b74c0ab8ec30376
chimny <aleks713@gmail.com>
2022-01-14 14:00:10 +0100

    refactoring calculateResult

--- src/tests/calculationSliceTests/calculateResult.test.js
+++ src/tests/calculationSliceTests/calculateResult.test.js
@@ -24,3 +24,27 @@ test('it should give result 13', () => {
 });
 
 
+test('it should update values - previous result as first number, secondnumber as payload and result based on two of them', () => {
+
+    const previousState = {
+        firstNumber: 13,
+        operator: '+',
+        secondNumber: 5,
+        result: 15,
+    };
+
+
+
+
+    expect(reducer(previousState, calculateResult('2'))).toEqual(
+        {  firstNumber: 15,
+            operator: '+',
+            secondNumber: 2,
+            result: 20,}
+    );
+
+
+
+});
+
+
