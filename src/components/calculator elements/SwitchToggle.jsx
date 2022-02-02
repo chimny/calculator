b2b449368096286f38ac93683593a9747befb727
chimny <aleks713@gmail.com>
2022-02-02 17:22:49 +0100

    before button component refactor

--- src/components/calculator elements/SwitchToggle.jsx
+++ src/components/calculator elements/SwitchToggle.jsx
@@ -10,25 +10,28 @@ export const SwitchToggle = () => {
     const setThemeFunction = (theme) => dispatch(setTheme(theme));
     const activeStatus = useSelector((state => state.theme.themeName));
 
-    const themeOption = [{spanClass:'firstOption',number:1},{spanClass:'secondOption',number:2},{spanClass:'thirdOption',number:3}]
-    const themeNames = [firstTheme.themeName,secondTheme.themeName,thirdTheme.themeName];
+    const themeOption = [{spanClass: 'firstOption', number: 1}, {
+        spanClass: 'secondOption',
+        number: 2
+    }, {spanClass: 'thirdOption', number: 3}]
+    const themeNames = [firstTheme.themeName, secondTheme.themeName, thirdTheme.themeName];
 
     return (
 
         <StyledSwitchToggle>
             <h2>theme</h2>
-            {themeOption.map(({spanClass,number})=>  <span className={spanClass}>{number}</span>)}
+            {themeOption.map(({spanClass, number}) => <span className={spanClass}>{number}</span>)}
 
 
             <div className='switchBox'>
 
-                {themeNames.map((theme)=>{
-                    return(
-                    <label className='container' htmlFor={theme} >
-                    <input id={theme} type="checkbox" checked={activeStatus === theme ?? false}
-                    onChange={() => setThemeFunction(theme)}/>
-                    <span className="checkmark"></span>
-                    </label>
+                {themeNames.map((theme) => {
+                    return (
+                        <label className='container' htmlFor={theme}>
+                            <input id={theme} type="checkbox" checked={activeStatus === theme ?? false}
+                                   onChange={() => setThemeFunction(theme)}/>
+                            <span className="checkmark"></span>
+                        </label>
                     )
                 })}
 
